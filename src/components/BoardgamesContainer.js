ec95e0011a20bcddb23992d5248f75a5e51b7538
Amanda <amandabaydar@gmail.com>
2021-10-11 16:01:15 -0400

    change handling of props

--- src/components/BoardgamesContainer.js
+++ src/components/BoardgamesContainer.js
@@ -3,22 +3,19 @@ import BoardgamesList from './BoardgamesList';
 
 const BoardgamesContainer = () => {
 
-    const [boardgames, setBoardgames] = useState("")
+    const [boardgames, setBoardgames] = useState(null)
 
     useEffect(() => {
         fetch('http://localhost:3000/')
         .then(res => res.json())
         .then(bgs => {
-            console.log("bgs", bgs)
             setBoardgames(bgs)
         })
     }, [])
 
-    console.log("boardgames", boardgames.boardgames)
-
         return (
             <div>
-                <BoardgamesList boardgames={boardgames.boardgames} />
+                <BoardgamesList boardgames={boardgames} />
             </div>
         )
 }
--- src/components/BoardgamesList.js
+++ src/components/BoardgamesList.js
@@ -2,17 +2,11 @@ import React from 'react';
 import BoardgamesForm from './BoardgamesForm';
 
 const BoardgamesList = (props) => {
-    console.log("props", props.boardgames)
+
         return (
             <div>
                 <BoardgamesForm />
-                {props.boardgames.map((bg) => {
-                    <ul>
-                        <li key={bg._id}>
-                            {bg.name}
-                        </li>
-                    </ul>
-                }) }
+
             </div>
         )
 }
